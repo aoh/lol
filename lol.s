1d90a19cc035e113337712be96d19e17a7a899dc
Aki Helin <aki.helin@iki.fi>
2011-03-22 23:04:29 +0200

    decided to use proper CPS instead of pushing frames to native stack. makes life easier with call/cc, multiple value returns, tailcalls and gc.

--- lol.s
+++ lol.s
@@ -1,6 +1,6 @@
 ## lol - low-level lisp
 
-#  Milestone 1: 
+#  Milestone 1: <- we're here
 #    - choose data representation, calling convention, etc
 #    - use manually translated written assembly with tagged data
 #    - check that it works
@@ -42,49 +42,36 @@
 #        o symbol = likewise, but interned and different tag
 #        o bignum = -||-
 #        o vector = not applicable (could use complete binary trees?)
-#  - bootup:
-#     + symbol interning will probably require some hackery
-#        o construct all in-heap values after startup?
-#        o have a builtin preloaded heap?
 #  - register use and calling conventions
-#     + use the native stack for parameter passing
-#        o probably push a marker to denote end of stack
-#        o have gc check that all roots in stack (values sans immediate bit) are in heap-range
-#           * for return addresses looking like pointers they will be above or below
-#     + each function call pushes the object (usually closure) implicitly to stack below the arguments
-#        o when the code starts to run, stack has [return-addr <clos> <a1> .. <an> return-addr' ...]
-#        o or it is kept in a separate register, which is pushed/popped on calls?
-#     + MUST SUPPORT CONSTANT SPACE TAILCALLS
-#     + pass all arguments in stack, and have special meaning for some registers
-#  - special registers
-#     + free pointer
-#     + closure (unless in stack)
-#     + nargs (only when making call)
-#     + return value (unless at top of stack (support multiple return values?))
-#     + continuation? (could autopush/pop like the closure, if necessary)
+#     + use CPS
+#     + push a register end marker to stack and keep only one frame (local variables) there
+#     + keep env in a separate register (?)
+#     + possibly also global env (?)
+#     + multi-value returns using normal calls
+#     + registers are preserved across calls (modulo moving fp etc)
+#        o clobberer fixes if necessary (syscalls)
 
 ## syscalls @ /usr/include/asm/unistd_32.h
 
-.comm toemit,4,4    # pointer to use for initial one-char writes
-.comm heap,10000,32 # 10000 words 
+.comm heap,100,32 # 100 words, 50 cells
+.comm toemit,4,4    # pointer to use for initial one-char writes (and end of heap)
 .text
 
-emit: # byte cont → cont val
-   movl $42, (toemit)           # assuming little endian machine here
+star: # preserve regs, emit a star (for debugging)
+   pushl %eax
+   pushl %ebx
+   pushl %ecx
+   pushl %edx
+   movl $42, (toemit)      # *
    movl $4, %eax           # syscall num (write)
    movl $1, %ebx           # fd
    movl $toemit, %ecx      # buf
    movl $1, %edx           # count
    int $0x80               # make syscall
-   ret
-
-nudder: # r n .. → 42 .., O(n)
-   cmpl $0, 4(%esp)
-   je nudder_out
-   subl $1, 4(%esp)
-   jmp nudder
-nudder_out:
-   movl $42, 4(%esp)
+   popl %ecx
+   popl %edx
+   popl %ebx
+   popl %eax
    ret
 
 adder: # r a b → n 
@@ -99,9 +86,26 @@ halt: # r a →
    movl 4(%esp), %ebx  # return value
    int $0x80           # exit
 
+init_freelist: # chain freelist to %esi
+   movl $heap, %esi    # pos
+   movl $50, %ecx      # counter, 50 cells in 100 words
+nextlist:
+   cmpl $0, %ecx     # maybe stop
+   je donelist
+   call star
+   movl %esi, %eax     # set pos+2words to cdr
+   addl $8, %eax
+   movl %eax, 4(%esi)
+   movl %eax, %esi
+   subl $1, %ecx
+   jmp nextlist
+donelist:
+   movl $heap, %esi
+   ret
+
 .globl _start
 _start:
-   pushl $10000000
-   call nudder
+   call init_freelist
+   pushl $42
    call halt
 
